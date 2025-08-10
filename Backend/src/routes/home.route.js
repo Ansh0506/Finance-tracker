3d7ead0ec087d141ebc0292b48afa6fad81aa1b5
Ansh kumar <anshresostudent123@gmail.com>
2025-08-10 15:51:38 +0530

    controllers update

--- Backend/src/routes/home.route.js
+++ Backend/src/routes/home.route.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import { Router } from 'express';
-import { registerController } from '../controllers/user.controller.js';
+import { registerController , loginUser} from '../controllers/user.controller.js';
 
 const router = Router();
 
 router.route("/register").post(registerController)
-
+router.route("/login").post(loginUser)
 
 export default router;
